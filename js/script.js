237b24f1fad55f1918fba41d73a4b5eaec27f882
jvorvick <49791235+jvorvick@users.noreply.github.com>
2019-08-11 12:14:15 -0700

    Code comments added, starter file comments deleted

--- js/script.js
+++ js/script.js
@@ -1,40 +1,14 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+// Global variables to be interacted with through functions
 
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 const pageDiv = document.querySelector('.page');
 const students = document.querySelector('.student-list').children;
 const studentsPerPage = 10;
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+/* 
+   Function to identify and display students corresponding to a given page.
+   Ten students will be displayed per page.
+   Students not belonging to the given page will be hidden. 
+*/
 function showPage(list, page) {  
    const startIndex = (page * 10) - 10;
    const endIndex = (page * 10) - 1;
@@ -47,12 +21,20 @@ function showPage(list, page) {
    }   
 }
 
+// The showPage function is called immediately after creation, and the first page is displayed.
+
 showPage(students, 1);
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+/* 
+   A div and unordered list for the students are created.
+   The ul is appended to the div.
+   Each student, in the form of an li, is appended to the ul in a loop.
+   Each li has a numbered hyperlink attached, representing each page number.
+   An event listener checks for a click on any of the ul's child lis. 
+   Upon a click event, the clicked lis will obtain a class of "active."
+   In addition, all other lis have their className set to null to ensure they are not "active."
+   The showPage function is called immediately afterword; the corresponding arguments are passed to ensure the clicked page is displayed.
+*/
 function appendPageLinks (list, page) {
    const div = document.createElement('div');
    div.className = 'pagination';
@@ -83,6 +65,6 @@ function appendPageLinks (list, page) {
    });
 }
 
-appendPageLinks(students, studentsPerPage);
+// appendPageLinks function is called to display the proper number of students on the currently active page
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+appendPageLinks(students, studentsPerPage);
